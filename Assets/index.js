9f3de7cb14fc5e4e7a3c03c738a37a27d7eb2eaf
Beast <95392136+KBailey-95@users.noreply.github.com>
2022-04-02 10:35:37 -0400

    Creation of questions for readme population and other essential functions.

--- Assets/index.js
+++ Assets/index.js
@@ -1,44 +1,88 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+//Setup
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
 
+//Questions
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
 
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is your project title?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'What is your project about?',
-    },
-    {
-      type: 'input',
-      name: 'install',
-      message: 'Are there any installation instructions?',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'What does your project do?',
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Are there any additional contributors you would like to list?',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Would you like to describe any tests in place for the application?',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Please choose the license we need to utilize.',
-      choices: ['a', 'b'],
-    },
-  ]);
-};
+// Async function to run program
+  async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
+
